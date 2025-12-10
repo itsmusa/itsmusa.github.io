a0f67f71fd48ab39ca4944177e972395945cb1fc
Msawenkosi Magwaza <65224007+itsmusa@users.noreply.github.com>
2025-12-10 10:27:27 +0200

    Delete js/modal.js

--- js/modal.js
+++
@@ -1,9 +0,0 @@
-const projectContentModal = document.getElementById("modal");
-
-const closeModal = () => {
-    projectContentModal.classList.remove('active');
-    if (projectContentModal.classList.contains('projects')) {
-        document.getElementById('modalContent').innerHTML = "";
-        console.log("removed content");
-    }
-}
